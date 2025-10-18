0c1511ad42b27fc90e6875924e27a8beda3aac48
Pingorin <125970228+Pingorin@users.noreply.github.com>
2025-10-18 10:16:29 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,10 +1,15 @@
-# File: info.py
+# info.py
 
-# BotFather से मिला हुआ अपना बॉट टोकन यहाँ डालें
-BOT_TOKEN = "YOUR_BOT_TOKEN" 
+import os
 
-# /start कमांड पर भेजी जाने वाली फोटो का डायरेक्ट URL
-PHOTO_URL = "https://telegra.ph/file/a048593a523a7852b6507.jpg"
+# Telegram API Credentials
+API_ID = os.environ.get("API_ID", 123456)  # अपना API ID यहाँ डालें
+API_HASH = os.environ.get("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e") # अपना API HASH यहाँ डालें
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "50a04ab22e0b3552dd351028a6d1a1c4") # अपना BOT TOKEN यहाँ डालें
 
-# इनलाइन बटन में दिखने वाला लिंक
-CHANNEL_LINK = "https://t.me/YourChannelName" # अपने चैनल का लिंक यहाँ डालें
+# Database
+DB_URI = os.environ.get("DB_URI", "mongodb://<user>:<password>@<host>:<port>/<db_name>") # अपना MongoDB Connection String यहाँ डालें
+
+# Other
+ADMINS = [int(admin) for admin in os.environ.get("ADMINS", "123456789").split()] # अपनी Telegram User ID यहाँ डालें (ये एडमिन होगी)
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-100xxxxxxxxx")) # Log भेजने के लिए चैनल ID
